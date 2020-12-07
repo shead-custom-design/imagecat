4ca36963aa20a89b812faec177e50ed269c0bc45
Timothy M. Shead <tim@shead-custom-design.com>
2020-12-07 08:25:58 -0700

    Simplify sphinx rtd theme config.

--- docs/conf.py
+++ docs/conf.py
@@ -81,14 +81,6 @@ on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = "sphinx_rtd_theme"
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-else:
-    html_context = {
-        "css_files": [
-            "https://media.readthedocs.io/css/sphinx_rtd_theme.css",
-            "https://media.readthedocs.io/css/readthedocs-doc-embed.css",
-        ],
-    }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
