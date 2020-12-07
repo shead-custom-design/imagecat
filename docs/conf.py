530c2694e700f8fe4ae410af939e4b1fe07b2c87
Timothy M. Shead <tim@shead-custom-design.com>
2020-12-07 08:42:57 -0700

    Further simplify docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -43,6 +43,9 @@ release = imagecat.__version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
+
+import sphinx_rtd_theme
+
 extensions = [
     "nbsphinx",
     "sphinx.ext.autodoc",
@@ -51,6 +54,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "sphinx_gallery.load_style",
+    "sphinx_rtd_theme",
 ]
 
 intersphinx_mapping = {
@@ -75,17 +79,13 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+#html_static_path = ["_static"]
 
 def warn_undocumented_members(app, what, name, obj, options, lines):
     if what not in [] and len(lines) == 0:
